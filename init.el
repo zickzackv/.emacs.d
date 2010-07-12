feed8b477ccdefc896456da3548fac8ef1ea60f5
zickzackv@betelgeuse.local <zickzackv@betelgeuse.local>
2010-07-12 22:49:17 +0200

    added diplom project
    * uses haskell, tex and bib files for a project
    * Directory must contain an Diplom.org file

--- init.el
+++ init.el
@@ -53,11 +53,15 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (require 'eproject)
+
 (define-project-type rails (generic)
   (look-for "Rakefile")
   :relevant-files ("\\.rb$" "\\.erb$" "\\.yml$" "\\.css$")
-  :irrelevant-files ("vendor/.*" "tmp/.*" "doc/.*" "log/.*" "script/.*")
-)
+  :irrelevant-files ("vendor/.*" "tmp/.*" "doc/.*" "log/.*" "script/.*"))
+
+(define-project-type diplom (generic)
+  (look-for "Diplom.org")
+  :relevant-files ("\\.tex$" "\\.bib$" "\\.hs$"))
       
 ;; use bookmarks easyly
 (require 'bm)
