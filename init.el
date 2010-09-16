e9af01b4afd348849ed5bdcdd3bb1ce65dfacf42
Fabian Otto <fabian.otto@familienservice.de>
2010-09-17 00:10:39 +0200

    changing color for files in anything

--- init.el
+++ init.el
@@ -50,7 +50,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Sending mail from within emacs to google
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 (setq starttls-use-gnutls t
 	  send-mail-function             'smtpmail-send-it
       message-send-mail-function     'smtpmail-send-it
@@ -379,20 +378,21 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(make-backup-files nil)  ; do not write backup files ( ./bli.foo~) 
- '(truncate-lines t)
  '(blink-cursor-mode t)
  '(column-number-mode t)
+ '(make-backup-files nil)
  '(recentf-max-saved-items 40)
  '(recentf-mode t)
  '(recentf-save-file "~/.emacs.d/recent-files")
  '(ruby-insert-encoding-magic-comment nil)
- '(show-paren-mode t))
+ '(show-paren-mode t)
+ '(truncate-lines t))
 
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(anything-file-name ((t (:foreground "White"))))
  '(cursor ((t (:background "blue" :foreground "black")))))
 
