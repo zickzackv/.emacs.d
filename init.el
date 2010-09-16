c7a7c7aa751dfb637f8388b5e19f2c2d555bfb0b
Fabian Otto <fabian.otto@gmail.com>
2010-09-17 00:14:29 +0200

    adds paredit (beta) with symlink

--- init.el
+++ init.el
@@ -174,6 +174,12 @@
 (require 'slime)
 (slime-setup '(slime-repl))
 
+(autoload 'paredit-mode "paredit"
+  "Minor mode for pseudo-structurally editing Lisp code." t)
+(add-hook 'emacs-lisp-mode-hook       (lambda () (paredit-mode +1)))
+(add-hook 'lisp-mode-hook             (lambda () (paredit-mode +1)))
+(add-hook 'lisp-interaction-mode-hook (lambda () (paredit-mode +1)))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Anything 
 (require 'anything-config)
