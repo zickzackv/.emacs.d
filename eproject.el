798265e32f19762af5d2ea26279496ae60399144
Fabian Otto <fabian.otto@familienservice.de>
2010-10-08 15:21:34 +0200

    Entfernt extjs aus rails Verzeichnissen. Speziell für Themis

--- eproject.el
+++ eproject.el
@@ -8,7 +8,7 @@
 (define-project-type rails (generic)
   (look-for "Rakefile")
   :relevant-files ("\\.rb$" "\\.erb$" "\\.yml$" "\\.css$")
-  :irrelevant-files ("vendor/.*" "tmp/.*" "doc/.*" "log/.*" "script/.*"))
+  :irrelevant-files ("vendor/.*" "tmp/.*" "doc/.*" "log/.*" "script/.*" "public/extjs/.*"))
 
 (define-project-type diplom (generic)
   (look-for "Diplom.org")
