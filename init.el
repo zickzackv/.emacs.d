4234e57ce2947242435d2452b67aa7fc62d137bd
Fabian Otto <fabian.otto@gmail.com>
2010-11-08 08:40:28 +0100

    Various changes of displaying line number
    - Adding aspell is not yet complete
    - adding autopair mode is not yet finished

--- init.el
+++ init.el
@@ -289,11 +289,24 @@
  '(truncate-lines t))
 
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(default ((t :inherit nil :stipple nil :background "#2e3434" :foreground "#eeeeec" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :)))
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil
+						 :background "#2e3434"
+						 :foreground "#eeeeec"
+						 :inverse-video nil
+						 :box nil
+						 :strike-through nil
+						 :overline nil
+						 :underline nil
+						 :slant normal
+						 :weight normal
+						 :height 130
+						 :width normal
+						 :foundry "apple"
+						 :family "Anonymous_Pro"))))
  '(anything-file-name ((t (:foreground "White"))))
  '(font-lock-string-face ((t (:foreground "#ad7fa8" :slant normal))))
  '(hl-line ((t (:background "black")))))
