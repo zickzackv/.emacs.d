f0c18d13418be07d3db163cf73c5ea377386fd5b
Fabian Otto <fabian.otto@familienservice.de>
2011-02-04 12:11:22 +0100

    Comments smart tab indentation out for ruby

--- init.el
+++ init.el
@@ -132,35 +132,35 @@
 
 (setq cua-auto-tabify-rectangles nil)
 
-(defadvice align (around smart-tabs activate)
-  (let ((indent-tabs-mode nil)) ad-do-it))
-(defadvice align-regexp (around smart-tabs activate)
-  (let ((indent-tabs-mode nil)) ad-do-it))
-(defadvice indent-relative (around smart-tabs activate)
-  (let ((indent-tabs-mode nil)) ad-do-it))
-(defadvice indent-according-to-mode (around smart-tabs activate)
-  (let ((indent-tabs-mode indent-tabs-mode))
-    (if (memq indent-line-function
-              '(indent-relative
-                indent-relative-maybe))
-        (setq indent-tabs-mode nil))
- ad-do-it))
+;; (defadvice align (around smart-tabs activate)
+;;   (let ((indent-tabs-mode nil)) ad-do-it))
+;; (defadvice align-regexp (around smart-tabs activate)
+;;   (let ((indent-tabs-mode nil)) ad-do-it))
+;; (defadvice indent-relative (around smart-tabs activate)
+;;   (let ((indent-tabs-mode nil)) ad-do-it))
+;; (defadvice indent-according-to-mode (around smart-tabs activate)
+;;   (let ((indent-tabs-mode indent-tabs-mode))
+;;     (if (memq indent-line-function
+;;               '(indent-relative
+;;                 indent-relative-maybe))
+;;         (setq indent-tabs-mode nil))
+;;  ad-do-it))
 
-(defmacro smart-tabs-advice (function offset)
-  (defvaralias offset 'tab-width)
-  `(defadvice ,function (around smart-tabs activate)
-     (cond
-      (indent-tabs-mode
-       (save-excursion
-         (beginning-of-line)
-         (while (looking-at "\t*\\( +\\)\t+")
-           (replace-match "" nil nil nil 1)))
-       (setq tab-width tab-width)
-       (let ((tab-width fill-column)
-                 (,offset fill-column))
-             ad-do-it))
-      (t
-       ad-do-it))))
+;; (defmacro smart-tabs-advice (function offset)
+;;   (defvaralias offset 'tab-width)
+;;   `(defadvice ,function (around smart-tabs activate)
+;;      (cond
+;;       (indent-tabs-mode
+;;        (save-excursion
+;;          (beginning-of-line)
+;;          (while (looking-at "\t*\\( +\\)\t+")
+;;            (replace-match "" nil nil nil 1)))
+;;        (setq tab-width tab-width)
+;;        (let ((tab-width fill-column)
+;;                  (,offset fill-column))
+;;              ad-do-it))
+;;       (t
+;;        ad-do-it))))
 
 (add-hook 'ruby-mode-hook (lambda ()
 			    (setq ruby-insert-encoding-magic-comment 't)))
