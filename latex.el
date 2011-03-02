76e6a4650421412e4f295f0d9ba3e5658f78a574
Fabian Otto <fabian.otto@gmail.com>
2011-03-02 14:21:21 +0100

    (auc)Tex Mode in my-german input method and deleting full ack

--- latex.el
+++ latex.el
@@ -1,13 +1,17 @@
 
 (require 'tex-site)
 
+
+;; set preview as pdf viewer.
 (setq-default TeX-view-program-list '(("Preview.app" "open -a Preview.app %o")))
 (setq-default TeX-view-program-selection '(((output-dvi style-pstricks) "Preview.app") 
 																					 (output-dvi "open") 
 																					 (output-pdf "Preview.app") 
 																					 (output-html "open")))
 
+;; german input and moving between paragraphs with M-n and M-p
 (add-hook 'TeX-mode-hook (lambda ()
+													 (set-input-method "my-german-postfix")
 													 (setq-default TeX-master nil) ; Query for master file.
 													 (TeX-fold-mode 1)
 													 (TeX-PDF-mode 1)
