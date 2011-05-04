51ae75e80b83d4c5931ea1f3f030f0e6fa85135b
Fabian Otto <fabian.otto@familienservice.de>
2011-05-04 20:52:32 +0200

    Adds customization file

---
+++ custom.el
@@ -0,0 +1,35 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#ad7fa8" "#8cc4ff" "#eeeeec"])
+ '(column-number-mode t)
+ '(cua-mode t nil (cua-base))
+ '(filesets-data (quote (("Themis" (:tree "~/Source/themis/" "^.+\\.rb$") (:filter-dirs-flag t)) ("Emacs Config" (:files "/Users/faot/.emacs.d/my-anything.el" "/Users/faot/.emacs.d/keyboard.el" "~/.emacs.d/display.el" "~/.emacs.d/init.el")))))
+ '(global-hl-line-mode nil)
+ '(gud-gdb-command-name "gdb --annotate=1")
+ '(indent-tabs-mode nil)
+ '(indicate-buffer-boundaries (quote right))
+ '(large-file-warning-threshold nil)
+ '(make-backup-files nil)
+ '(next-line-add-newlines t)
+ '(recentf-max-saved-items 100)
+ '(recentf-mode t)
+ '(recentf-save-file "~/.emacs.d/recent-files")
+ '(ruby-insert-encoding-magic-comment nil)
+ '(safe-local-variable-values (quote ((TeX-master . "../diplom") (TeX-master . t) (fill-colum . 100) (Package . User) (Syntax . Common-Lisp) (Package . CCL) (encoding . utf-8))))
+ '(sentence-end-double-space nil)
+ '(show-paren-mode t)
+ '(tool-bar-mode nil)
+ '(truncate-lines t))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(message "custom-file loaded")
+
